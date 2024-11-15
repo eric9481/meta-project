9a186d43973cd6552b660ac9c349078a06fa7fa1
Eric <eric.smith.9481@gmail.com>
2024-11-15 02:46:35 -0700

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import logoAlt from '../assets/logoVar.jpg'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+    <div class="bg-container">
+      <div className="footer container">
+        <img src={logoAlt} className='footer-logo' alt='little lemon logo'/>
+        <p className='footer-copyright'>copyright Little-Lemon 2024</p>
+        
+          <ul className='footer-nav'>
+            <li><a>Home</a></li>
+            <li><a>About</a></li>
+            <li><a>Menu</a></li>
+            <li><a>Reservations</a></li>
+            <li><a>Order Online</a></li>
+            <li><a>Login</a></li>
+          </ul>
+          <ul className='footer-nav'>
+            <li>Address</li>
+            <li>Phone</li>
+            <li>Email</li>
+          </ul>
+          <ul className='footer-nav'>
+            <li>Address</li>
+            <li>Phone</li>
+            <li>Email</li>
+          </ul>
+        </div>
+      </div>
+    
+    
+    </>
   )
 }
 
